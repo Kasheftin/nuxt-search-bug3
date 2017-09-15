185385cc5be325e96d5eb6d771ea509cf275148c
Kasheftin <Kasheftin@gmail.com>
2017-09-15 12:51:28 +0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        nuxt-bug3
+      </h1>
+      <h2 class="subtitle">
+        Nuxt.js project
+      </h2>
+      <div class="links">
+        <nuxt-link class="button--green" to="/srchComponent">Go to srchComponent in the root, it has the bug</nuxt-link>
+        <nuxt-link class="button--grey" to="/nuxtChildWrapper/srchComponent">Go to srchComponent wrapped in nuxt-child, there's no bug</nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ pages/srchComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>Try to type something in the input field below.</p>
+    <p>If this component called as the root, like /srchComponent, it will reinintialize after each key press.</p>
+    <p>If this component wrapped in the nuxt-child, it will work as expected without constant reinitializations.</p>
+    <input v-model="srch">
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      srch: this.$route.query.srch
+    }
+  },
+  watch: {
+    srch (srch) {
+      this.$router.push({name: this.$route.name, query: {srch}})
+    }
+  },
+  created () {
+    console.log('Component created:', this.$route.name)
+  },
+  beforeDestroy () {
+    console.log('Component is about to be destroyed:', this.$route.name)
+  }
+}
+</script>
